0c61b3850133e43fe941d351fa2095857a8e263c
Mayowa Ajiboye <mayaajiboye@gmail.com>
2022-12-19 20:10:36 -0500

    commit tree de1e9b8240daf81af23ab9b7efd9ba51a66d48a3

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+  employee.name = 'Sam'
+
+  const copyEmployee = {...employee}
+  copyEmployee[key] = value
+  return copyEmployee;
+}
+
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee.streetAddress = '12 Broadway'
+    return employee
+}
+
+function  deleteFromEmployeeByKey(employee, key){
+  const newEmployee = {...employee}
+  delete newEmployee.name
+
+  return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+  delete employee.name;
+  return employee;
+}
+
